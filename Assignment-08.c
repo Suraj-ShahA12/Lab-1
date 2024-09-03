70cb8ec28c767a6b18b79ee13e6776b60a623f59
Suraj-ShahA12 <i24ai012@aid.svnit.ac.in>
2024-09-03 04:45:55 -0700

    Update Assignment-08.c

--- Assignment-08.c
+++ Assignment-08.c
@@ -6,4 +6,5 @@ int main()
 	hr=(31558150-(3600*24*day))/3600;
 	min=(31558150-(3600*24*day)-(3600*hr))/60;
 	printf("Earth takes %d days, %d hours and %d minutes to revolve around the Sun",day,hr,min);
+	return 0;
 }
